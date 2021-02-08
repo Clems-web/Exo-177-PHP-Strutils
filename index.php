93a904064c2c0edc62505899905763559851c418
Clems-web <clement.servais1@gmail.com>
2021-02-08 10:45:56 +0100

    Exercice terminé

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 
 // TODO n'oubliez pas de créer votre classe dans le dossier classes.
 
+include "./classes.php";
+
+$str1 = new classes("une chaîne de caractère");
+
+$str1->bold($str1);
+echo "<br>";
+$str1->ital($str1);
+echo "<br>";
+$str1->underline($str1);
+$str1->caps($str1);
+$str1->uglify($str1);
+
